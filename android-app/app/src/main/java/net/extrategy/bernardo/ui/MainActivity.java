9542bcc5edf8fa34dce011196f05ac30810bc892
Saverio Tosi <saverio.tosi@ideato.info>
2019-02-19 23:14:04 +0100

    Adds new layout

--- android-app/app/src/main/java/net/extrategy/bernardo/ui/MainActivity.java
+++ android-app/app/src/main/java/net/extrategy/bernardo/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package net.extrategy.bernardo;
+package net.extrategy.bernardo.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import net.extrategy.bernardo.R;
 
 public class MainActivity extends AppCompatActivity {
 
